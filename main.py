8888f8299889e3f275dafe5902342c46ed5ef739
Sam Naylor <s.naylor5@newcastle.ac.uk>
2021-05-08 18:44:51 +0100

    Add a unit-test

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ f
             j
 """
 
-from typing import Optional
+from typing import Optional, Iterator
 
 
-class CustomIter:
+class CustomIter(Iterator):
     def __init__(self, source: str):
         self.source: list[str] = list(source)
 
@@ -31,41 +31,66 @@ class CustomIter:
             raise StopIteration()
 
 
-source = CustomIter(__doc__)
+def tokenise(code: str) -> list[str]:
+    source = CustomIter(code)
 
-level = 0
-levels: list[int] = []
-tokens: list[str] = []
+    level = 0
+    levels: list[int] = []
+    tokens: list[str] = []
 
-while True:
-    try:
-        char = next(source)
-        if char == "\n":
-            lvl = 0
-            
-            while (source.peek() == " "):
-                lvl += 1
-                next(source)
-            
-            if lvl > level:
-                tokens.append("indent")
-                levels.append(lvl)
-                level = lvl
+    while True:
+        try:
+            char = next(source)
+            if char == "\n":
+                lvl = 0
+                
+                while (source.peek() == " "):
+                    lvl += 1
+                    next(source)
+                
+                if lvl > level:
+                    tokens.append("indent")
+                    levels.append(lvl)
+                    level = lvl
 
-            while lvl < level:
-                tokens.append("dedent")
-                levels.pop(-1)
-                if len(levels):
-                    level = levels[-1]
-                else:
-                    level = 0
+                while lvl < level:
+                    tokens.append("dedent")
+                    levels.pop(-1)
+                    if len(levels):
+                        level = levels[-1]
+                    else:
+                        level = 0
 
-                if level < lvl:
-                    raise Exception("Invalid indentation level")
-        else:
-            tokens.append(char)
-    except StopIteration:
-        break
+                    if level < lvl:
+                        raise Exception("Invalid indentation level")
+            else:
+                tokens.append(char)
+        except StopIteration:
+            break
+
+    return tokens
 
 
-print(tokens)
+def test_tokenise():
+    assert tokenise(__doc__) == [
+        "a",
+        "indent",
+        "b",
+        "c",
+        "indent",
+        "d",
+        "dedent",
+        "e",
+        "dedent",
+        "f",
+        "indent",
+        "g",
+        "h",
+        "indent",
+        "i",
+        "indent",
+        "j",
+        "dedent",
+        "dedent",
+        "dedent"
+    ]
